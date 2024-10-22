d07f9bfdc1d7881a0a5a1414e17222fc5a65720a
luke-hamann <135341515+luke-hamann@users.noreply.github.com>
2024-10-21 19:53:53 -0500

    Add database and models

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Image, Tag, ImageTag
+
+admin.site.register(Image)
+admin.site.register(Tag)
+admin.site.register(ImageTag)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Image(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.CharField(max_length=1023)
+    date = models.DateTimeField()
+    url = models.CharField(max_length=255)
+
+class Tag(models.Model):
+    name = models.CharField(max_length=80)
+
+class ImageTag(models.Model):
+    image = models.ForeignKey(Image, on_delete=models.CASCADE)
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
