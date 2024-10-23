cede97292669f0bb1d1b1e2cde0f018f83d0726a
luke-hamann <135341515+luke-hamann@users.noreply.github.com>
2024-10-23 08:51:29 -0500

    Add thumbnails; Improve templates

--- main/views.py
+++ main/views.py
@@ -1,10 +1,8 @@
 from datetime import datetime
-
+import io
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
-
 import PIL.Image
-
 from .models import Image, Tag
 from .forms import ImageForm
 
@@ -32,13 +30,28 @@ def image(request, image_id):
     image = get_object_or_404(Image, pk=image_id)
     format = request.GET.get('format', 'original')
 
-    copy = PIL.Image.open(image.file).copy()
-    with PIL.Image.open(image.file) as file:
-        content_type = file.get_format_mimetype()
-        
-    return HttpResponse(image.file, content_type=content_type)
+    im = PIL.Image.open(image.file)
+
+    content_type = im.get_format_mimetype()
+    file_extension = content_type.split("/")[1]
+
+    buffer = io.BytesIO()
+
+    if (format == 'thumbnail'):
+        im.thumbnail((128, 128))
+
+    im.save(buffer, file_extension)
+    content = buffer.getvalue()
+
+    return HttpResponse(content, content_type=content_type)
 
 def search(request):
+    query = request.GET.get('q', '')
     images = Image.objects.all()
 
-    return render(request, 'search.html', {'images': images})
+    context = {
+        'query': query,
+        'images': images,
+    }
+
+    return render(request, 'search.html', context)
