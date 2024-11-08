d2e70c61d62d024e529e0bad93a35cbd101ba57c
luke-hamann <135341515+luke-hamann@users.noreply.github.com>
2024-11-08 15:50:44 -0600

    Fix up JS

--- main/static/js/autocomplete.js
+++ main/static/js/autocomplete.js
@@ -1,15 +1,27 @@
 (() => {
-    var inputElement = document.querySelector('.search-box');
+    const SEARCH_BOX = '.search-box';
+    const SUGGESTION_LIST = '#suggestionList';
+    const SUGGESTION_ITEMS = '.suggestionItem';
+    const SELECTED_SUGGESTION = 'selectedSuggestion';
+
+    // Initialize the variables
+    var inputElement = document.querySelector(SEARCH_BOX);
     var autocompleteTimeout;
     var currentValue = inputElement.value;
     var selectedIndex = -1;
 
+    /**
+     * Remove the autocompletion menu
+     */
     function removeAutocomplete() {
         selectedIndex = -1;
-        var suggestionList = document.querySelector('#suggestionList');
+        var suggestionList = document.querySelector(SUGGESTION_LIST);
         if (suggestionList != null) suggestionList.remove();
     }
 
+    /**
+     * Render the autocompletion menu
+     */
     function renderAutocomplete() {
         var query = encodeURIComponent(inputElement.value);
         fetch(`/autocomplete/?q=${query}`)
@@ -17,10 +29,10 @@
         .then((text) => {
             removeAutocomplete();
 
-            // Recreate the selection list if necessary
+            // Rebuild the menu
             var parser = new DOMParser();
             var dom = parser.parseFromString(text, 'text/html');
-            var suggestionList = dom.querySelector('#suggestionList');
+            var suggestionList = dom.querySelector(SUGGESTION_LIST);
             var count = Number(suggestionList.getAttribute('data-count'));
 
             if (count == 0) return;
@@ -28,8 +40,9 @@
             inputElement.insertAdjacentElement('afterEnd', suggestionList);
 
             var suggestionItems =
-                [...suggestionList.querySelectorAll('.suggestionItem')];
+                [...suggestionList.querySelectorAll(SUGGESTION_ITEMS)];
 
+            // Add a click event to each suggestion item
             for (var item of suggestionItems) {
                 item.addEventListener('click', (event) => {
                     selectedIndex = -1;
@@ -49,48 +62,53 @@
         setTimeout(removeAutocomplete, 100);
     });
 
+    /**
+     * Render the autocomplete menu everytime the user changes the search text
+     */
     inputElement.addEventListener('input', () => {
         selectedIndex = -1;
         currentValue = inputElement.value;
+
+        // Debounce the input events
         clearTimeout(autocompleteTimeout);
         autocompleteTimeout = setTimeout(renderAutocomplete, 50);
     });
 
+    /**
+     * Manage keyboard navigation through the suggestion list
+     */
     inputElement.addEventListener('keydown', (event) => {
         if (!['ArrowUp', 'ArrowDown'].includes(event.key)) return;
         event.preventDefault();
 
-        suggestionList = document.querySelector('#suggestionList');
+        suggestionList = document.querySelector(SUGGESTION_LIST);
         if (suggestionList == null) return;
 
-        var resultElements = document.querySelectorAll('#suggestionList > div');
-        var choices = [];
-        for (var element of resultElements) {
-            choices.push(element.innerText);
-        }
+        var suggestionElements =
+            [...document.querySelectorAll(SUGGESTION_ITEMS)];
 
+        // Remove selection from previously selected element
         if (selectedIndex > -1) {
-            var selectedElement = resultElements[selectedIndex];
-            selectedElement.classList.remove('selectedSuggestion');
+            var selectedElement = suggestionElements[selectedIndex];
+            selectedElement.classList.remove(SELECTED_SUGGESTION);
         }
 
-        if (event.key == 'ArrowDown') selectedIndex++;
-        else if (event.key == 'ArrowUp') selectedIndex--;
-        if (selectedIndex >= choices.length) {
+        // Compute the new selection index
+        selectedIndex += (event.key == 'ArrowDown' ? 1 : -1);
+
+        if (selectedIndex >= suggestionElements.length) {
             selectedIndex = -1;
         } else if (selectedIndex < -1) {
-            selectedIndex = choices.length - 1;
+            selectedIndex = suggestionElements.length - 1;
         }
 
+        // Update the input field and selected suggestion
         if (selectedIndex > -1) {
-            inputElement.value = choices[selectedIndex];
+            var selectedElement = suggestionElements[selectedIndex];
+            selectedElement.classList.add(SELECTED_SUGGESTION);
+            inputElement.value = selectedElement.innerText;
         } else {
             inputElement.value = currentValue;
-        }
-
-        if (selectedIndex > -1) {
-            var selectedElement = resultElements[selectedIndex];
-            selectedElement.classList.add('selectedSuggestion');
         }
 
         var index = inputElement.value.length;
--- main/static/js/autotag.js
+++ main/static/js/autotag.js
@@ -1,59 +1,9 @@
 (() => {
     var controller = new AbortController();
 
-    async function autotagImage() {
-        var [file] = fileInputElement.files;
-        if (file) {
-            controller.abort();
-            controller = new AbortController();
-            previewImage.src = URL.createObjectURL(file);
-
-            // Set up the loading icon
-            var label = document.querySelector('[for="id_tags"]');
-            var loadingIndicator = document.createElement('span');
-            label.appendChild(loadingIndicator);
-            var state = 0;
-            var icons = ['🌑', '🌒', '🌓', '🌔', '🌕', '🌖', '🌗', '🌘'];
-            var interval = setInterval(() => {
-                loadingIndicator.innerText = ' ' + icons[state];
-                state++;
-                state %= icons.length;
-            }, 100);
-
-            // Set up the image upload
-            upload = new File([await file.arrayBuffer()], file.name, {
-                'type': file.type
-            });
-            var data = new FormData();
-            data.append('csrfmiddlewaretoken', CSRF_TOKEN);
-            data.append('file', upload);
-
-            // Post the image
-            fetch('/autotag/', {
-                signal: controller.signal,
-                method: 'POST',
-                body: data
-            })
-            .then(response => response.json())
-            .then(json => {
-                if (tagsInputElement.value != '') {
-                    tagsInputElement.value += ' ';
-                }
-                tagsInputElement.value += json.join(' ');
-            })
-            .finally(() => {
-                clearInterval(interval);
-                loadingIndicator.remove();
-            });
-        } else {
-            previewImage.src = initialSource;
-            suggestions = '';
-        }
-    }
-
     // Get the DOM elements
     var fileInputElement = document.querySelector('[name=file]');
-    var tagsInputElement = document.querySelector('[name=tags]')
+    var tagsInputElement = document.querySelector('[name=tags]');
 
     // Select or create the preview image element
     var previewImage = document.querySelector('.image-preview');
@@ -68,5 +18,55 @@
     }
 
     // Change the preview image on a file change
-    fileInputElement.addEventListener('change', autotagImage);
+    fileInputElement.addEventListener('change', async () => {
+        var [file] = fileInputElement.files;
+
+        if (!file) {
+            previewImage.src = initialSource;
+            suggestions = '';
+            return;
+        }
+
+        controller.abort();
+        controller = new AbortController();
+        previewImage.src = URL.createObjectURL(file);
+
+        // Set up the loading icon
+        var label = document.querySelector('[for="id_tags"]');
+        var loadingIndicator = document.createElement('span');
+        label.appendChild(loadingIndicator);
+        var state = 0;
+        var icons = ['🌑', '🌒', '🌓', '🌔', '🌕', '🌖', '🌗', '🌘'];
+        var interval = setInterval(() => {
+            loadingIndicator.innerText = ' ' + icons[state];
+            state++;
+            state %= icons.length;
+        }, 100);
+
+        // Set up the image upload
+        upload = new File([await file.arrayBuffer()], file.name, {
+            'type': file.type
+        });
+        var data = new FormData();
+        data.append('csrfmiddlewaretoken', CSRF_TOKEN);
+        data.append('file', upload);
+
+        // Post the image
+        fetch('/autotag/', {
+            signal: controller.signal,
+            method: 'POST',
+            body: data
+        })
+        .then(response => response.json())
+        .then(json => {
+            if (tagsInputElement.value != '') {
+                tagsInputElement.value += ' ';
+            }
+            tagsInputElement.value += json.join(' ');
+        })
+        .finally(() => {
+            clearInterval(interval);
+            loadingIndicator.remove();
+        });
+    });
 })()
