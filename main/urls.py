cd22207b0981728508d3fcc51f669ae6d4436e86
luke-hamann <135341515+luke-hamann@users.noreply.github.com>
2024-11-11 14:04:51 -0600

    Add comment

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-
 from . import views
 
 
+# Define the URL routing for the app
 urlpatterns = [
     path('', views.search, name='home'),
     path('tags/', views.tags, name='tags'),
@@ -13,5 +13,5 @@ urlpatterns = [
     path('detail/<int:image_id>/', views.detail, name='detail'),
     path('detail/<int:image_id>/<str:slug>/', views.detail, name='detail'),
     path('detail/<int:image_id>/<str:slug>/edit/', views.edit, name='edit'),
-    path('detail/<int:image_id>/<str:slug>/delete/', views.delete, name='delete'),
+    path('detail/<int:image_id>/<str:slug>/delete/', views.delete, name='delete')
 ]
